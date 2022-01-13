2c0d482fa09ed6ca0afba74b707de6c15113b1d2
Cedric Diop <cedirc@reesykle.com>
2022-01-13 15:41:42 -0500

    Update property listing page details

--- src/pages/property.tsx
+++ src/pages/property.tsx
@@ -304,130 +304,11 @@ const Property: FC<ListingStayDetailPageProps> = ({className = "",isPreviewMode,
         );
     };
 
-    const renderSectionCheckIndate = () => {
-        return (
-            <div className="listingSection__wrap overflow-hidden">
-                {/* HEADING */}
-                <div>
-                    <h2 className="text-2xl font-semibold">Availability</h2>
-                    <span className="block mt-2 text-neutral-500 dark:text-neutral-400">
-            Prices may increase on weekends or holidays
-          </span>
-                </div>
-                <div className="w-14 border-b border-neutral-200 dark:border-neutral-700"></div>
-                {/* CONTENT */}
-
-                <div className="listingSection__wrap__DayPickerRangeController flow-root">
-                    <div className="-mx-4 sm:mx-auto xl:mx-[-22px]">
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
-    const renderSection5 = () => {
-        return (
-            <div className="listingSection__wrap">
-                {/* HEADING */}
-                <h2 className="text-2xl font-semibold">Host Information</h2>
-                <div className="w-14 border-b border-neutral-200 dark:border-neutral-700"></div>
-
-                {/* host */}
-                <div className="flex items-center space-x-4">
-                    <Avatar
-                        hasChecked
-                        hasCheckedClass="w-4 h-4 -top-0.5 right-0.5"
-                        sizeClass="h-14 w-14"
-                        radius="rounded-full"
-                    />
-                    <div>
-                        <a className="block text-xl font-medium" href="##">
-                            Kevin Francis
-                        </a>
-                        <div className="mt-1.5 flex items-center text-sm text-neutral-500 dark:text-neutral-400">
-                            <span className="mx-2">·</span>
-                            <span> 12 places</span>
-                        </div>
-                    </div>
-                </div>
-
-                {/* desc */}
-                <span className="block text-neutral-6000 dark:text-neutral-300">
-          Providing lake views, The Symphony 9 Tam Coc in Ninh Binh provides
-          accommodation, an outdoor swimming pool, a bar, a shared lounge, a
-          garden and barbecue facilities...
-        </span>
-
-                {/* info */}
-                <div className="block text-neutral-500 dark:text-neutral-400 space-y-2.5">
-                    <div className="flex items-center space-x-3">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={1.5}
-                                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                            />
-                        </svg>
-                        <span>Joined in March 2016</span>
-                    </div>
-                    <div className="flex items-center space-x-3">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={1.5}
-                                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                            />
-                        </svg>
-                        <span>Response rate - 100%</span>
-                    </div>
-                    <div className="flex items-center space-x-3">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={1.5}
-                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                        </svg>
-
-                        <span>Fast response - within a few hours</span>
-                    </div>
-                </div>
-
-                {/* == */}
-                <div className="w-14 border-b border-neutral-200 dark:border-neutral-700"></div>
-                <div>
-                    <ButtonSecondary href="##">See host profile</ButtonSecondary>
-                </div>
-            </div>
-        );
-    };
-
     const renderSection6 = () => {
         return (
             <div className="listingSection__wrap">
                 {/* HEADING */}
-                <h2 className="text-2xl font-semibold">Reviews (23 reviews)</h2>
+                <h2 className="text-2xl font-semibold">Real Time Chat</h2>
                 <div className="w-14 border-b border-neutral-200 dark:border-neutral-700"></div>
 
                 {/* Content */}
@@ -470,52 +351,22 @@ const Property: FC<ListingStayDetailPageProps> = ({className = "",isPreviewMode,
         return (
             <div className="listingSection__wrap">
                 {/* HEADING */}
-                <h2 className="text-2xl font-semibold">Things to know</h2>
+                <h2 className="text-2xl font-semibold">Remodeling Details</h2>
                 <div className="w-14 border-b border-neutral-200 dark:border-neutral-700" />
 
                 {/* CONTENT */}
                 <div>
-                    <h4 className="text-lg font-semibold">Cancellation policy</h4>
+                    <h4 className="text-lg font-semibold">Sustainability</h4>
                     <span className="block mt-3 text-neutral-500 dark:text-neutral-400">
-            Refund 50% of the booking value when customers cancel the room
-            within 48 hours after successful booking and 14 days before the
-            check-in time. <br />
-            Then, cancel the room 14 days before the check-in time, get a 50%
-            refund of the total amount paid (minus the service fee).
-          </span>
+                        Here's how this property will be remodeled to meet our sustainability requirements.
+                    </span>
+                    <br/>
+                    <ul>
+                        <li>Solar Panels</li>
+                        <li>Energy Efficiency Upgrades</li>
+                    </ul>
                 </div>
                 <div className="w-14 border-b border-neutral-200 dark:border-neutral-700" />
-
-                {/* CONTENT */}
-                <div>
-                    <h4 className="text-lg font-semibold">Check-in time</h4>
-                    <div className="mt-3 text-neutral-500 dark:text-neutral-400 max-w-md text-sm sm:text-base">
-                        <div className="flex space-x-10 justify-between p-3 bg-neutral-100 dark:bg-neutral-800 rounded-lg">
-                            <span>Check-in</span>
-                            <span>08:00 am - 12:00 am</span>
-                        </div>
-                        <div className="flex space-x-10 justify-between p-3">
-                            <span>Check-out</span>
-                            <span>02:00 pm - 04:00 pm</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="w-14 border-b border-neutral-200 dark:border-neutral-700" />
-
-                {/* CONTENT */}
-                <div>
-                    <h4 className="text-lg font-semibold">Special Note</h4>
-                    <div className="prose sm:prose">
-                        <ul className="mt-3 text-neutral-500 dark:text-neutral-400 space-y-2">
-                            <li>
-                                Ban and I will work together to keep the landscape and
-                                environment green and clean by not littering, not using
-                                stimulants and respecting people around.
-                            </li>
-                            <li>Do not sing karaoke past 11:30</li>
-                        </ul>
-                    </div>
-                </div>
             </div>
         );
     };
@@ -638,8 +489,6 @@ const Property: FC<ListingStayDetailPageProps> = ({className = "",isPreviewMode,
                     {renderSection2()}
                     {renderSection3()}
                     {renderSection4()}
-                    {renderSectionCheckIndate()}
-                    {renderSection5()}
                     {renderSection6()}
                     {renderSection7()}
                     {renderSection8()}
@@ -674,7 +523,7 @@ const Property: FC<ListingStayDetailPageProps> = ({className = "",isPreviewMode,
                     <div className="relative py-16">
                         <BackgroundSection />
                         <NewCategoriesSliderSection
-                            heading="Explore by types of stays"
+                            heading="Explore Properties by Type"
                             subHeading="Explore houses based on 10 types of stays"
                             categoryCardType="card5"
                             itemPerRow={5}

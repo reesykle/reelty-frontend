7b2938bb97df56b2c7d4f5a0f5288843d0cd15fb
Cedric Diop <95982162+cedricdiop@users.noreply.github.com>
2022-02-09 02:13:13 -0500

    Add listing page (#4)
    * Remove unused component
    
    * Remove unused filters
    
    * Add listing page
    
    * Update property listing page details
    
    * Update text
    
    Co-authored-by: Cedric Diop <cedirc@reesykle.com>

--- src/components/Filters/TabFilters.tsx
+++ src/components/Filters/TabFilters.tsx
@@ -4,7 +4,6 @@ import ButtonPrimary from "../../shared/Button/ButtonPrimary";
 import ButtonThird from "../../shared/Button/ButtonThird";
 import ButtonClose from "../../shared/ButtonClose/ButtonClose";
 import Checkbox from "../../shared/CheckBox/Checkbox";
-import convertNumbThousand from "../../utils/convertNumbThousand";
 import { Range } from "rc-slider";
 import NcInputNumber from "../NcInputNumber/NcInputNumber";
 
@@ -104,129 +103,6 @@ const TabFilters = () => {
           </>
         )}
       </Popover>
-    );
-  };
-
-  const renderTabsPriceRage = () => {
-    return (
-      <Popover className="relative">
-        {({ open, close }) => (
-          <>
-            <Popover.Button
-              className={`flex items-center justify-center px-4 py-2 text-sm rounded-full border border-primary-500 bg-primary-50 text-primary-700 focus:outline-none `}
-            >
-              <span>
-                {`$${convertNumbThousand(
-                  rangePrices[0]
-                )} - $${convertNumbThousand(rangePrices[1])}`}{" "}
-              </span>
-              {renderXClear()}
-            </Popover.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute z-10 w-screen max-w-sm px-4 mt-3 left-0 sm:px-0 ">
-                <div className="overflow-hidden rounded-2xl shadow-xl bg-white dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-700">
-                  <div className="relative flex flex-col px-5 py-6 space-y-8">
-                    <div className="space-y-5">
-                      <span className="font-medium">Price per day</span>
-                      <Range
-                        min={0}
-                        max={2000}
-                        defaultValue={[0, 1000]}
-                        allowCross={false}
-                        onChange={setRangePrices}
-                      />
-                    </div>
-
-                    <div className="flex justify-between space-x-5">
-                      <div>
-                        <label
-                          htmlFor="minPrice"
-                          className="block text-sm font-medium text-neutral-700 dark:text-neutral-300"
-                        >
-                          Min price
-                        </label>
-                        <div className="mt-1 relative rounded-md">
-                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <span className="text-neutral-500 sm:text-sm">
-                              $
-                            </span>
-                          </div>
-                          <input
-                            type="text"
-                            name="minPrice"
-                            disabled
-                            id="minPrice"
-                            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-3 sm:text-sm border-neutral-200 rounded-full text-neutral-900"
-                            value={rangePrices[0]}
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <label
-                          htmlFor="maxPrice"
-                          className="block text-sm font-medium text-neutral-700 dark:text-neutral-300"
-                        >
-                          Max price
-                        </label>
-                        <div className="mt-1 relative rounded-md">
-                          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <span className="text-neutral-500 sm:text-sm">
-                              $
-                            </span>
-                          </div>
-                          <input
-                            type="text"
-                            disabled
-                            name="maxPrice"
-                            id="maxPrice"
-                            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-3 sm:text-sm border-neutral-200 rounded-full text-neutral-900"
-                            value={rangePrices[1]}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="p-5 bg-neutral-50 dark:bg-neutral-900 dark:border-t dark:border-neutral-800 flex items-center justify-between">
-                    <ButtonThird onClick={close} sizeClass="px-4 py-2 sm:px-5">
-                      Clear
-                    </ButtonThird>
-                    <ButtonPrimary
-                      onClick={close}
-                      sizeClass="px-4 py-2 sm:px-5"
-                    >
-                      Apply
-                    </ButtonPrimary>
-                  </div>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </>
-        )}
-      </Popover>
-    );
-  };
-
-  const renderTabOnSale = () => {
-    return (
-      <div
-        className={`flex items-center justify-center px-4 py-2 text-sm rounded-full border focus:outline-none cursor-pointer transition-all ${
-          isOnSale
-            ? "border-primary-500 bg-primary-50 text-primary-700"
-            : "border-neutral-300 dark:border-neutral-700"
-        }`}
-        onClick={() => setIsOnSale(!isOnSale)}
-      >
-        <span>On sale</span>
-        {isOnSale && renderXClear()}
-      </div>
     );
   };
 
@@ -421,16 +297,10 @@ const TabFilters = () => {
                   </div>
 
                   <div className="p-6 flex-shrink-0 bg-neutral-50 dark:bg-neutral-900 dark:border-t dark:border-neutral-800 flex items-center justify-between">
-                    <ButtonThird
-                      onClick={closeModalMoreFilter}
-                      sizeClass="px-4 py-2 sm:px-5"
-                    >
+                    <ButtonThird onClick={closeModalMoreFilter} sizeClass="px-4 py-2 sm:px-5">
                       Clear
                     </ButtonThird>
-                    <ButtonPrimary
-                      onClick={closeModalMoreFilter}
-                      sizeClass="px-4 py-2 sm:px-5"
-                    >
+                    <ButtonPrimary onClick={closeModalMoreFilter} sizeClass="px-4 py-2 sm:px-5">
                       Apply
                     </ButtonPrimary>
                   </div>
@@ -447,12 +317,9 @@ const TabFilters = () => {
     <div className="flex lg:space-x-4">
       <div className="hidden lg:flex space-x-4">
         {renderTabsTypeOfPlace()}
-        {renderTabsPriceRage()}
-        {renderTabOnSale()}
       </div>
       <div className="flex lg:hidden space-x-4">
         {renderTabMobileFilter()}
-        {renderTabOnSale()}
       </div>
     </div>
   );
---
+++ src/shared/Avatar/Avatar.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+
+export interface AvatarProps {
+  containerClassName?: string;
+  sizeClass?: string;
+  radius?: string;
+  imgUrl?: string;
+  userName?: string;
+  hasChecked?: boolean;
+  hasCheckedClass?: string;
+}
+
+const avatarColors = [
+  "#ffdd00",
+  "#fbb034",
+  "#ff4c4c",
+  "#c1d82f",
+  "#f48924",
+  "#7ac143",
+  "#30c39e",
+  "#06BCAE",
+  "#0695BC",
+  "#037ef3",
+  "#146eb4",
+  "#8e43e7",
+  "#ea1d5d",
+  "#fc636b",
+  "#ff6319",
+  "#e01f3d",
+  "#a0ac48",
+  "#00d1b2",
+  "#472f92",
+  "#388ed1",
+  "#a6192e",
+  "#4a8594",
+  "#7B9FAB",
+  "#1393BD",
+  "#5E13BD",
+  "#E208A7",
+];
+
+export { avatarColors };
+
+
+const Avatar: React.FC<AvatarProps> = ({
+  containerClassName = "ring-1 ring-white dark:ring-neutral-900",
+  sizeClass = "h-6 w-6 text-sm",
+  radius = "rounded-full",
+  imgUrl = "/images/avatars/Image-1.png",
+  userName,
+  hasChecked,
+  hasCheckedClass = "w-4 h-4 -top-0.5 -right-0.5",
+}) => {
+  const url = imgUrl || "";
+  const name = userName || "John Doe";
+  const _setBgColor = (name: string) => {
+    const backgroundIndex = Math.floor(
+      name.charCodeAt(0) % avatarColors.length
+    );
+    return avatarColors[backgroundIndex];
+  };
+
+  return (
+    <div
+      className={`wil-avatar relative flex-shrink-0 inline-flex items-center justify-center text-neutral-100 uppercase font-semibold shadow-inner ${radius} ${sizeClass} ${containerClassName}`}
+      style={{ backgroundColor: url ? undefined : _setBgColor(name) }}
+    >
+      {url && (
+        <img
+          className={`absolute inset-0 w-full h-full object-cover ${radius}`}
+          src={url}
+          alt={name}
+        />
+      )}
+      <span className="wil-avatar__name">{name[0]}</span>
+
+      {hasChecked && (
+        <span
+          className={` bg-teal-500 rounded-full text-white text-xs flex items-center justify-center absolute  ${hasCheckedClass}`}
+        >
+          <i className="las la-check"></i>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Avatar;
